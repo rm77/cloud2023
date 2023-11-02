dfa303fe1d634c73ac2e1971e52945009229ff7c
roy <royyana@gmail.com>
2023-11-02 17:29:50 +0700

    update to newer image

--- environment/script/vm-lib/images/download.sh
+++ environment/script/vm-lib/images/download.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-URL=https://cloud-images.ubuntu.com/releases/groovy/release-20210720/ubuntu-20.10-server-cloudimg-amd64.img
+#URL=https://cloud-images.ubuntu.com/releases/groovy/release-20210720/ubuntu-20.10-server-cloudimg-amd64.img
+URL=https://cloud-images.ubuntu.com/mantic/current/mantic-server-cloudimg-amd64.img
 curl -Lo cloud-image.img $URL
 
