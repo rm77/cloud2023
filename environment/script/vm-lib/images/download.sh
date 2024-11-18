721a567524d3a2be53ef0e5279ab99cacfeb48d4
Daniel Pepuho <69733783+danielcristho@users.noreply.github.com>
2024-11-18 21:46:46 +0700

    feat: Update download.sh
    change url.

--- environment/script/vm-lib/images/download.sh
+++ environment/script/vm-lib/images/download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #URL=https://cloud-images.ubuntu.com/releases/groovy/release-20210720/ubuntu-20.10-server-cloudimg-amd64.img
-URL=https://cloud-images.ubuntu.com/mantic/current/mantic-server-cloudimg-amd64.img
+URL=https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
 curl -Lo cloud-image.img $URL
 
