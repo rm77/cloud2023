846c2f48e9113440eba543b5ca5fe185beea2266
royyana <royyana@gmail.com>
2025-10-09 09:23:24 +0700

    edit

--- environment/script/orchestration/run.sh
+++ environment/script/orchestration/run.sh
@@ -1,6 +1,9 @@
 docker run \
 	-d \
+	--privileged \
 	--name cfy  \
+	--cap-add=SYS_RAWIO \
+	--cap-add=ALL \
 	-v /sys/fs/cgroup:/sys/fs/cgroup:ro --tmpfs /run --tmpfs /run/lock \
-	-p 40080:80 cloudifyplatform/community-cloudify-manager-aio:latest
+	-p 30080:80 cloudifyplatform/community-cloudify-manager-aio:latest
 	#-v $(pwd)/config.yaml:/etc/cloudify/config.yaml:rw \
